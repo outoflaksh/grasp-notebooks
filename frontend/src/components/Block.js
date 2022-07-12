92de1db4d617ae98b5a6a7d5447648bc5531330e
yattew <yathansh@gmail.com>
2022-07-12 22:55:36 +0530

    basic output block done, api connectivity remaining

--- frontend/src/components/Block.js
+++ frontend/src/components/Block.js
@@ -4,20 +4,38 @@ import runCodeIcon from "../assets/runCode.png"
 
 function Block() {
     const codeBlockContent = useRef();
+    const [output, setOutput] = useState("this is the output");
 
-    function runCode(){
+    function runCode() {
         let content = codeBlockContent.current.innerHTML;
-        let sanitizedCode = content.replace( /(<([^>]+)>)/ig, '');
+        let sanitizedCode = content.replace(/(<([^>]+)>)/ig, '');
         console.log(sanitizedCode);
     }
-
+    function clearOutput(){
+        setOutput("");
+    }
     return (
-        <div className="code-block">
-            <div className="code-block-info">
-                <button className="run-code-button" onClick={runCode}><img src={runCodeIcon}/></button>
+        <div className="block">
+            <div className="code-block">
+                <div className="code-block-info">
+                    <button className="run-code-button" onClick={runCode}><img src={runCodeIcon} /></button>
+                </div>
+                <pre className="code-block-content" contentEditable="true" ref={codeBlockContent} spellCheck="false">
+                </pre>
             </div>
-            <pre className="code-block-content" contentEditable="true" ref={codeBlockContent} spellCheck="false">
-            </pre>
+            {
+                output.length > 0 ?
+                    <div className="output-block">
+                        <div className="output-block-info">
+                            {/* <span className="output-status">[ ]</span> */}
+                            <span className="clear-output" onClick={clearOutput}>[ CLEAR ]</span>
+                        </div>
+                        <div className="output-text">
+                            {output}
+                        </div>
+                    </div>
+                    : null
+            }
 
         </div>
     );
