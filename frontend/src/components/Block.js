3ef9b4ab26345a0d87697f462eaa9fb63aa6fee8
yattew <yathansh@gmail.com>
2022-07-15 20:35:41 +0530

    output table implimented, design left

--- frontend/src/components/Block.js
+++ frontend/src/components/Block.js
@@ -1,14 +1,31 @@
 import { useRef, useState } from "react";
 import "../styles/block.css"
 import runCodeIcon from "../assets/runCode.png"
+import OutputTable from "./OutputTable";
+
+
+function sqlizeData(data) {
+    let columns = [];
+    for (let column in data[0]) {
+        columns.push(column);
+    }
+    let matrix = [[...columns]];
+    data.shift();
+    for (let r of data) {
+        let row = [];
+        for (let col_name of columns) {
+            row.push(r[col_name]);
+        }
+        matrix.push(row);
+    }
+    return matrix;
+}
+
 
 
 async function executeQuery(code) {
 
     const requestBody = JSON.stringify({ query: code });
-
-    console.log(requestBody);
-
     const response = await fetch("http://localhost:8000/run/", {
         method: "POST",
         mode: "cors",
@@ -35,10 +52,17 @@ function Block() {
         let content = codeBlockContent.current.innerHTML;
         let sanitizedCode = content.replace(/(<([^>]+)>)/ig, '');
         let data = await executeQuery(sanitizedCode);
-        let output = "";
-        data.result.forEach((element) => { output += element });
+        let newOutput = "";
+        if (data.status) {
+            newOutput = sqlizeData(data.result);
+            // console.log("sqlized data",newOutput);
+            setOutput(newOutput);
+        }
+        else {
+            newOutput = data.result[0];
+            setOutput(newOutput);
+        }
         setOutputStatus(data.status);
-        setOutput(output);
     }
     function clearOutput() {
         setOutput("");
@@ -57,22 +81,41 @@ function Block() {
                 </pre>
             </div>
             {
-                output.length > 0 ?
-                    <div
-                        className={
-                            "output-block" +
-                            " " +
-                            (outputStatus ? "output-success" : "output-error")
-                        }
-                    >
-                        <div className="output-block-info">
-                            <span className="clear-output" onClick={clearOutput}>[ CLEAR ]</span>
-                        </div>
-                        <div className="output-text">
-                            {output}
-                        </div>
-                    </div>
-                    : null
+                (() => {
+                    if (output.length > 0) {
+                        return (
+                            <div
+                                className={
+                                    "output-block" +
+                                    " " +
+                                    (outputStatus ? "output-success" : "output-error")
+                                }
+                            >
+                                <div className="output-block-info">
+                                    <span className="clear-output" onClick={clearOutput}>[ CLEAR ]</span>
+                                </div>
+                                {
+                                    (() => {
+                                        if (outputStatus) {
+                                            return <OutputTable data={output}></OutputTable>;
+                                        }
+                                        else {
+                                            return (
+                                                <div className="output-text">
+                                                    {output}
+                                                </div>
+                                            );
+                                        }
+                                    })()
+                                }
+
+                            </div>
+                        );
+                    }
+                    else {
+                        return null;
+                    }
+                })()
             }
 
         </div>
---
+++ frontend/src/components/OutputTable.js
@@ -0,0 +1,26 @@
+
+
+function OutputTable({ data }) {
+    // console.log(data);
+    let headingArr = data[0];
+    let bodyArr = [...data];
+    bodyArr.shift();
+    return (
+        <table>
+            <thead>
+                <tr>
+                    {headingArr.map(col => <td key={col}>{col}</td>)}
+                </tr>
+            </thead>
+            <tbody>
+                {bodyArr.map(row =>
+                    <tr key={"row".concat(row[0])}>
+                        {row.map(el => <td key={"col".concat(el)}>{el}</td>)}
+                    </tr>
+                )}
+            </tbody>
+        </table>
+    );
+}
+
+export default OutputTable;
