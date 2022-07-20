98914654f7c61bca81751f0973adf86e4a6c85a2
laksh <lakshyam38@gmail.com>
2022-07-20 11:05:35 +0530

    Separate API+Backend logic folder

---
+++ api/api.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+from .logic.sql_handler import run_sql_query
+
+app = FastAPI(debug=True)
+
+notebook: list = None
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class Query(BaseModel):
+    query: str
+
+
+@app.post("/run")
+def run_query(query: Query):
+    result = run_sql_query(query.query)
+
+    return {"result": result[1], "status": result[0]}
+
+
+@app.post("/save")
+def save(new_notebook: list):
+    global notebook
+    notebook = new_notebook
+    print("saving\n", notebook)
+    return {"result": "saved"}
+
+
+@app.get("/notebook")
+def notebook():
+    print("getting\n", notebook)
+    return {"notebook": notebook}
