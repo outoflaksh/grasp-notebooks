2c63de4902ff736215d29e219f9e715ca32faa9b
laksh <lakshyam38@gmail.com>
2022-07-24 13:51:27 +0530

    Add functionality to save nb content as JSON files

--- api/routers/notebook.py
+++ api/routers/notebook.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 
 from ..models import Notebook
+from ..utils.notebook_utils import save_nb_data, load_saved_nb_data
 
 router = APIRouter()
 
@@ -11,6 +12,8 @@ notebooks = []
 def save_notebook(notebook: Notebook):
     notebooks.append(dict(notebook))
 
+    save_nb_data(nb_data=notebook.data, nb_id=notebook.id)
+
     return {"detail": "Notebook saved."}
 
 
@@ -18,6 +21,7 @@ def save_notebook(notebook: Notebook):
 def read_notebook_by_id(notebook_id: str):
     for nb in notebooks:
         if nb["id"] == notebook_id:
+            load_saved_nb_data(nb_id=nb["id"])
             return nb
 
     raise HTTPException(
---
+++ api/utils/notebook_utils.py
@@ -0,0 +1,27 @@
+import pickle
+import pathlib
+
+BASE_DIR = pathlib.Path(__file__).parent.parent
+UPLOAD_DIR = BASE_DIR / "uploads"
+
+print(UPLOAD_DIR)
+
+
+def save_nb_data(nb_data: list, nb_id: str):
+    UPLOAD_DIR.mkdir(exist_ok=True)
+
+    upload_path = UPLOAD_DIR / f"{nb_id}.pkl"
+    with open(upload_path, "wb") as f:
+        pickle.dump(nb_data, f)
+
+    print("Saved successfully")
+
+
+def load_saved_nb_data(nb_id: str):
+    upload_path = UPLOAD_DIR / f"{nb_id}.pkl"
+    with open(upload_path, "rb") as f:
+        data = pickle.load(f)
+
+    print("Retrieved data:", data)
+
+    return data
